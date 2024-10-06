4e250beb64aa4f292a127c6a52d53a1d1a73c301
Obinna Ogbonna <obinnaogbonnajoseph@gmail.com>
2024-10-06 01:17:20 +0100

    add documentation

--- ImportEngine/ImportInterface.py
+++ ImportEngine/ImportInterface.py
@@ -8,16 +8,40 @@ class ImportInterface(ABC):
     allowed_extensions = []
 
     @classmethod
-    def can_ingest(cls, path) -> bool:
+    def can_ingest(cls, path: str) -> bool:
+        """Check if the file can be parsed, based on the allowed extensions
+
+        Args:
+            path (str): file path
+
+        Returns:
+            bool: Can the file be parsed
+        """
         ext = path.split('.')[-1]
         return ext in cls.allowed_extensions
 
     @classmethod
     @abstractmethod
     def parse(cls, path: str) -> List[Cat]:
+        """Abstract method to parse a file to return a list of Cat objects
+
+        Args:
+            path (str): file path
+
+        Returns:
+            List[Cat]: list of Cat objects
+        """
         pass
 
     @staticmethod
     def parse_str(string: str) -> Cat:
+        """Static method to parse a string to return a Cat object
+
+        Args:
+            string (str): String to parse: 'name, age, is_indoor'
+
+        Returns:
+            Cat: a Cat object
+        """
         parse = string.split(',')
         return Cat(parse[0], int(parse[1]), bool(parse[2]))
--- ImportEngine/Importer.py
+++ ImportEngine/Importer.py
@@ -8,10 +8,18 @@ from .PDFImporter import PDFImporter
 
 
 class Importer(ImportInterface):
-    importers = [DocxImporter, CSVImporter, PDFImporter]
+    importers: List[ImportInterface] = [DocxImporter, CSVImporter, PDFImporter]
 
     @classmethod
     def parse(cls, path: str) -> List[Cat]:
+        """Parses a file to return a list of Cat objects
+
+        Args:
+            path (str): file path
+
+        Returns:
+            List[Cat]: list of Cat objects
+        """
         for importer in cls.importers:
             if importer.can_ingest(path):
                 return importer.parse(path)
